146965d991362c5a3ade0b73ead1c63198770e27
blablabla <sergiy.shpak@gmail.com>
2021-11-09 22:27:50 -0500

    kinda works ugly.. need to do better outer cycle but fuk it

--- legalinsurrection.py
+++ legalinsurrection.py
@@ -59,28 +59,51 @@ looks like inverted_order is useless?
 longLista={}
 counterLimit =40
 last_nid=""
+share="IDONTKNOW"
 
+#############   OMG THIS SHIT   range (1,15)   I AM SOO LAZY but fuk it
 
-#############   OMG THIS SHIT   range (1,50)   I AM SOO LAZY but fuk it
-
-for aaa in range (1,50):
+for aaa in range (1,20):
     
+    #day1
+    #URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2911033984355166890/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    
+    #day2
+    #URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2911778297784733795/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    
+    #day3  some mess in legai insur... says day 3 or 4
+    #URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2912512992021483619/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    
+    #day4 says day 4-2   some stupid charmap err
+    #URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2913251050483116694/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    
+    #day5
+    URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2915432986346688614/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    
+    #day6
     URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2916125192049037403/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    
+    
+    
     response = requests.get(URLO)
     parsed_json = json.loads(response.text)
-    currentListSize=len(parsed_json['data']['news'])
     
-       
     if parsed_json['err_code']==100:
+        currentListSize=len(parsed_json['data']['news'])
+        share=last_nid=parsed_json['data']['news'][0]['share']
         for i in range(0, currentListSize-1):
-            longLista[parsed_json['data']['news'][i]['created']]=parsed_json['data']['news'][i]['contents']
+            longLista[parsed_json['data']['news'][i]['created']]=parsed_json['data']['news'][i]['newstitle']+"<br>"+parsed_json['data']['news'][i]['contents']
             last_nid=parsed_json['data']['news'][i]['nid']
-            print("dict size")
-            print(len(longLista))
+
+
+print (len(longLista))
 
 fout= open("transcript.html","w+")
-fout.write('<html><body>')    
+fout.write('<html><body>')  
+fout.write(share)
+fout.write("<BR>")
    
+
 for key in sorted(longLista):
     fout.write(datetime.datetime.utcfromtimestamp(key).strftime('%Y-%m-%dT%H:%M:%SZ'))
     fout.write(longLista[key])
