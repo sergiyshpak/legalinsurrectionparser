ec0a4c00a1e6f73817d8952e3dea4fb13ec56410
blablabla <sergiy.shpak@gmail.com>
2021-11-09 21:19:27 -0500

    first working shit

--- legalinsurrection.py
+++ legalinsurrection.py
@@ -57,70 +57,26 @@ looks like inverted_order is useless?
 
 
 longLista={}
-
-
-#  shit it paged by 50 entries????  and that web page gets them by 10
-counter =40
-
+counterLimit =40
 last_nid=""
 
 
+#############   OMG THIS SHIT   range (1,50)   I AM SOO LAZY but fuk it
 
-
-#retenhouse day 6
-URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2916125192049037403/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counter)+"&origin=https%253A%252F%252Flegalinsurrection.com"
-
-response = requests.get(URLO)
-parsed_json = json.loads(response.text)
-
-#print(parsed_json)
-
-# {"err_code": 100, "err_msg": "Success", "data": {"news": [{"nid": "2916406705150782110", "newstitle": "Court Recesses for the Day", "contents": "<p><br><\/p>", 
-
-#print(parsed_json['data']['news'][3])
-
-
-
-
-
-
-
-
-print (counter)
-print ("-----------------------------")
-print(parsed_json['data']['news'][counter-i]['nid'])
-print ("-----------------------------")
-
-for i in range(1, counter+1):
+for aaa in range (1,50):
     
-    #print(counter-i)
+    URLO="https://data.24liveplus.com/v1/retrieve_server/x/event/2916125192049037403/news/?inverted_order=1&last_nid="+last_nid+"&limit="+str(counterLimit)+"&origin=https%253A%252F%252Flegalinsurrection.com"
+    response = requests.get(URLO)
+    parsed_json = json.loads(response.text)
+    currentListSize=len(parsed_json['data']['news'])
     
-    #print(parsed_json['data']['news'][counter-i])
-    """
-    {'nid': '2916406618640257128', 'newstitle': '', 
-    'contents': '<p><span style="background-color: rgba(0, 0, 0, 0.03); color: rgb(15, 20, 25);">That\'s it for defense witness Zanin.</span></p><p><br></p><p><span style="background-color: rgba(0, 0, 0, 0.03); color: rgb(15, 20, 25);">Court recesses for the day.</span></p>', 
-    'created': 1636497735, 'lastupdate': 0, 'uid': '2748969211805265551', 
-    'editorname': 1, 'uname': 'andrew', 'avatar': 'avatar/2748969211805265551/20210329135608_015360.png',
-    'avatar_url': 'https://24liveblog.tradingfront.cn/avatar/2748969211805265551/20210329135608_015360.png', 
-    'news_type': 0, 'likes': 0, 'cmts_all_count': 0, 
-    'share': 'https://legalinsurrection.com/2021/11/live-rittenhouse-trial-day-6/', 'duration': 0, 
-    'liveid': '', 
-    'background': '', 
-    'action_info': {}, 
-    'interval': 5, 'options': [], 
-    'voting_count': 0, 'comments_count': 0, 'comments': [], 
-    'admin_level': 1, 'widget': {}}
-    """
-    
-   ##### print(datetime.datetime.utcfromtimestamp(parsed_json['data']['news'][counter-i]['created']).strftime('%Y-%m-%dT%H:%M:%SZ'))
-    #print(datetime.utcfromtimestamp(parsed_json['data']['news'][counter-i]['created']).strftime('%Y-%m-%d %H:%M:%S'))
-    ####  print(parsed_json['data']['news'][counter-i]['contents'])
-    
-    
-    longLista[parsed_json['data']['news'][counter-i]['created']]=parsed_json['data']['news'][counter-i]['contents']
-
-    
- 
+       
+    if parsed_json['err_code']==100:
+        for i in range(0, currentListSize-1):
+            longLista[parsed_json['data']['news'][i]['created']]=parsed_json['data']['news'][i]['contents']
+            last_nid=parsed_json['data']['news'][i]['nid']
+            print("dict size")
+            print(len(longLista))
 
 fout= open("transcript.html","w+")
 fout.write('<html><body>')    
